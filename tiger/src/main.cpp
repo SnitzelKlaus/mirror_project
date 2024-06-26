db442269ab403b90444293189716497426c2c80b
Rasmus Kristensen <87420491+RTK-Umbraco@users.noreply.github.com>
2024-06-26 10:13:48 +0200

    Light senor + Braccio movement to find sun

--- tiger/src/main.cpp
+++ tiger/src/main.cpp
@@ -1,33 +1,134 @@
-#include <Arduino.h>
+  #include <Braccio.h>
+  #include <Servo.h>
 
-void setup() {
-  Serial.begin(19200);
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-
-}
-
-void loop() {
-
-  int sensor0Value = analogRead(A0);
-  float voltage0= sensor0Value * (5.0 / 1023.0);
-
-  int sensor1Value = analogRead(A1);
-  float voltage1= sensor1Value * (5.0 / 1023.0);
-  
-  int sensor2Value = analogRead(A2);
-  float voltage2= sensor2Value * (5.0 / 1023.0);
-  
-  int sensor3Value = analogRead(A3);
-  float voltage3= sensor3Value * (5.0 / 1023.0);
+  Servo base;
+  Servo shoulder;
+  Servo elbow;
+  Servo wrist_rot;
+  Servo wrist_ver;
+  Servo gripper;
 
 
-  Serial.println((String) "0:" + voltage0);
-  Serial.println((String) "1:" + voltage1);
-  Serial.println((String) "2:" + voltage2);
-  Serial.println((String) "3:" + voltage3);
+  void setup() {
+    // Setup serial communication at baudrate 9600 for reading the light sensor
+    Serial.begin(9600);
 
-  delay(100);
-}
+    Braccio.begin();
+
+    Braccio.ServoMovement(20, 0, 90, 90, 90, 90, 10);
+  }
+
+
+
+
+  long map(long x, long in_min, long in_max, long out_min, long out_max) {
+      return (long)( (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min );
+  }
+
+  long GetCalibrateLightValue(long lightValue){
+    long minValue = 4;
+    long maxValue = 1023;
+
+    // Serial.println("lightValue");
+    // Serial.println(lightValue);
+
+    long lightSenorValue = map(lightValue, minValue, maxValue, 0, 10000);
+
+    return lightSenorValue;
+  }
+
+
+  void PrintLightValues(long lightValueAnalogZero, long lightValueAnalogOne, long lightValueAnalogTwo, long lightValueAnalogThree){
+
+    Serial.println("Mapped light sensor value: ");
+    //Serial.print(lightValue);
+    Serial.println("A0");
+    Serial.println(lightValueAnalogZero);
+
+    Serial.println("A1");  
+    Serial.println(lightValueAnalogOne);
+
+    Serial.println("A2");
+    Serial.println(lightValueAnalogTwo);
+    
+    Serial.println("A3");
+    Serial.println(lightValueAnalogThree);
+  }
+
+  bool ValidateLightValues(long lightValueAnalogZero, long lightValueAnalogOne, long lightValueAnalogTwo, long lightValueAnalogThree) {
+    long maxValue = 10000;
+    return (lightValueAnalogZero == maxValue &&
+            lightValueAnalogOne == maxValue &&
+            lightValueAnalogTwo == maxValue &&
+            lightValueAnalogThree == maxValue);
+  }
+
+  int stepDelay = 20;
+  int moveBase = 100;
+  int moveShoulder = 90;
+  int moveElbow = 90;
+  void MovesBraccioTowardsTheHighestLightValue(long lightValueAnalogZero, long lightValueAnalogOne, long lightValueAnalogTwo, long lightValueAnalogThree){
+    // Move the Braccio arm towards the highest light sensor value
+    if (lightValueAnalogZero + 10 <= lightValueAnalogThree) {
+      // Move towards A0
+          Serial.println("Move toward A0");
+          moveElbow = moveElbow + 1;
+      // Braccio.ServoMovement(20, 30, 90, 180, 90, 90, 10); // Move base towards A0 direction
+    } 
+    else if (lightValueAnalogZero >= lightValueAnalogThree + 10){
+      // Move towards A3
+      Serial.println("Move toward A3");
+      moveElbow = moveElbow - 1;
+      // Braccio.ServoMovement(20, 30, 90, 0, 90, 90, 10); // Move shoulder towards A3 direction
+    }
+    if (lightValueAnalogOne + 10 <= lightValueAnalogTwo) {
+      // Move towards A1
+          Serial.println("Move toward A1");
+          if (moveBase < 0){
+            moveBase = 175;
+          }
+          else{
+            moveBase = moveBase - 1;
+          }
+          Serial.println(moveBase);
+
+      // Braccio.ServoMovement(20, 140, 90, 0, 90, 90, 10); // Move base towards A1 direction
+    } else if (lightValueAnalogOne >= lightValueAnalogTwo + 10)  {
+      // Move towards A2
+          Serial.println("Move toward A2");
+          if (moveBase >= 180){
+            moveBase = 5;
+          }
+          else{
+            moveBase = moveBase + 1;
+          }
+          Serial.println(moveBase);
+
+          
+    }
+      // Braccio.ServoMovement(20, 140, 90, 180, 90, 90, 10); // Move shoulder towards A2 direction
+    Braccio.ServoMovement(stepDelay, moveBase, moveShoulder, moveElbow, 90, 90, 10);
+
+  }
+
+  void MoveBracioMovement(){
+    int stepDelay = 20;
+    int base = 180;
+    int shoulder = 90;
+    int elbow = 90;
+
+    Braccio.ServoMovement(stepDelay, base, shoulder, elbow, 90, 90, 10);
+  }
+
+  void loop() {
+    long lightValueAnalogZero = GetCalibrateLightValue(analogRead(A0));
+    long lightValueAnalogOne = GetCalibrateLightValue(analogRead(A1));
+    long lightValueAnalogTwo = GetCalibrateLightValue(analogRead(A2));
+    long lightValueAnalogThree = GetCalibrateLightValue(analogRead(A3));
+
+    PrintLightValues(lightValueAnalogZero, lightValueAnalogOne, lightValueAnalogTwo, lightValueAnalogThree);
+    
+    MovesBraccioTowardsTheHighestLightValue(lightValueAnalogZero, lightValueAnalogOne, lightValueAnalogTwo, lightValueAnalogThree);
+
+    delay(100);
+  }
